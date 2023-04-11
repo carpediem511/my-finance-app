7ac6edbdd1618b054584531501c6d4a69792226d
carpediem511 <vampire.0514@gmail.com>
2023-04-11 23:13:51 +0300

    prettier

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,114 +5,120 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { v4 as uuidv4 } from "uuid";
 import Error from "components/Error";
-import ru from 'date-fns/locale/ru';
+import ru from "date-fns/locale/ru";
 
 const categories = [
-	{ value: "health", label: "Здоровье", image: "icon-helth.png" },
-	{ value: "finance", label: "Накопления", image: "icon-finance.png" },
-	{ value: "education", label: "Образование", image: "icon-education.png" },
-	{ value: "products", label: "Продукты", image: "icon-groceries.png" },
-	{ value: "travel", label: "Путешествия", image: "icon-travel.png" },
-	{ value: "entertainment", label: "Развлечения", image: "icon-entertainment.png", },
+  { value: "health", label: "Здоровье", image: "icon-helth.png" },
+  { value: "finance", label: "Накопления", image: "icon-finance.png" },
+  { value: "education", label: "Образование", image: "icon-education.png" },
+  { value: "products", label: "Продукты", image: "icon-groceries.png" },
+  { value: "travel", label: "Путешествия", image: "icon-travel.png" },
+  {
+    value: "entertainment",
+    label: "Развлечения",
+    image: "icon-entertainment.png",
+  },
 ];
 
 const ExpensesForm = ({ addNewSpending }) => {
-	const [enterAmount, setEnterAmount] = useState("");
-	const [chooseCategory, setChooseCategory] = useState(categories[0]);
-	const [startDate, setStartDate] = useState();
-	let [isOpen, setIsOpen] = useState(false);
+  const [enterAmount, setEnterAmount] = useState("");
+  const [chooseCategory, setChooseCategory] = useState(categories[0]);
+  const [startDate, setStartDate] = useState();
+  let [isOpen, setIsOpen] = useState(false);
 
-	const AddPurchaseFunction = (event) => {
-		event.preventDefault();
+  const AddPurchaseFunction = (event) => {
+    event.preventDefault();
 
-		// проверка на обязательное заполнение всех полей
-		if (chooseCategory && startDate && enterAmount) {
-			const purchase = {
-				category: chooseCategory.label,
-				price: enterAmount,
-				date: startDate,
-				id: uuidv4(),
-				image: chooseCategory.image,
-			};
+    // проверка на обязательное заполнение всех полей
+    if (chooseCategory && startDate && enterAmount) {
+      const purchase = {
+        category: chooseCategory.label,
+        price: enterAmount,
+        date: startDate,
+        id: uuidv4(),
+        image: chooseCategory.image,
+      };
 
-			addNewSpending(purchase);
-			setChooseCategory(categories[0]);
-			setEnterAmount("");
-			setStartDate(null);
-			setIsOpen(false);
-		} else {
-			setIsOpen(true);
-			return;
-		}
-	};
+      addNewSpending(purchase);
+      setChooseCategory(categories[0]);
+      setEnterAmount("");
+      setStartDate(null);
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+      return;
+    }
+  };
 
-	return (
-		<>
-			<form onSubmit={AddPurchaseFunction} className="w-full px-4 my-1 mx-auto">
-				<div className="my-8 flex flex-col gap-y-4">
-					<h2 className="text-4xl pt-4 pb-8 text-center font-['Caveat']">
-						Добавить новую покупку
-					</h2>
+  return (
+    <>
+      <form onSubmit={AddPurchaseFunction} className="w-full px-4 my-1 mx-auto">
+        <div className="my-8 flex flex-col gap-y-4">
+          <h2 className="text-4xl pt-4 pb-8 text-center font-['Caveat']">
+            Добавить новую покупку
+          </h2>
 
-					<div className="px-4 flex justify-around">
-						<div className="flex flex-col text-lg font-medium w-1/3">
-							<label className="font-semibold text-center text-indigo-600">Выбрать категорию</label>
-							<Select
-								options={categories}
-								value={chooseCategory}
-								onChange={(category) => setChooseCategory(category)}
-								className="mt-3 mx-auto py-3 w-2/3 focus:border-2 rounded-md"
-							>
-								{categories.map((category) => (
-									<option key={category}>{category}</option>
-								))}
-							</Select>
-						</div>
-						<div className="flex flex-col text-lg font-medium w-1/3">
-							<label className="font-semibold mt-4 mb-1 text-center text-indigo-600">
-								Выбрать месяц покупки
-							</label>
-							<DatePicker
-								selected={startDate}
-								onChange={(date) => setStartDate(date)}
-								locale={ru}
-								dateFormat="dd.MM.yyyy"
-								isClearable
-								placeholderText="	Пока здесь пусто"
-								className="border flex mx-auto mt-1 border-solid rounded-md outline-none border-gray-300 px-2 py-2 focus:border-2 focus:border-sky-500"
-							/>
-						</div>
+          <div className="px-4 flex justify-around">
+            <div className="flex flex-col text-lg font-medium w-1/3">
+              <label className="font-semibold text-center text-indigo-600">
+                Выбрать категорию
+              </label>
+              <Select
+                options={categories}
+                value={chooseCategory}
+                onChange={(category) => setChooseCategory(category)}
+                className="mt-3 mx-auto py-3 w-2/3 focus:border-2 rounded-md"
+              >
+                {categories.map((category) => (
+                  <option key={category}>{category}</option>
+                ))}
+              </Select>
+            </div>
+            <div className="flex flex-col text-lg font-medium w-1/3">
+              <label className="font-semibold mt-4 mb-1 text-center text-indigo-600">
+                Выбрать месяц покупки
+              </label>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                locale={ru}
+                dateFormat="dd.MM.yyyy"
+                isClearable
+                placeholderText="	Пока здесь пусто"
+                className="border flex mx-auto mt-1 border-solid rounded-md outline-none border-gray-300 px-2 py-2 focus:border-2 focus:border-sky-500"
+              />
+            </div>
 
-						<div className="mb-1 flex flex-col text-lg font-medium w-1/3">
-							<label className="font-semibold mb-1 mt-4 text-center text-indigo-600">
-								Ввести потраченную сумму
-							</label>
-							<input
-								name="enterAmount"
-								value={enterAmount}
-								onChange={(event) => {
-									const value = event.target.value;
-									// ограничение ввода только цифрами
-									const pattern = /^[0-9]{1,9}$/;
-									if (pattern.test(value)) {
-										setEnterAmount(value);
-									}
-								}}
-								type="text"
-								className="border pl-2 mt-1 border-solid rounded-md outline-none border-gray-300 mx-auto py-2 focus:border-2 focus:border-sky-500"
-							/>
-						</div>
-					</div>
-					<ButtonAdd
-						title="Добавить"
-						type="submit"
-						AddPurchaseFunction={AddPurchaseFunction}
-					/>
-					<Error isOpen={isOpen} setIsOpen={setIsOpen} />
-				</div>
-			</form>
-		</>
-	);
+            <div className="mb-1 flex flex-col text-lg font-medium w-1/3">
+              <label className="font-semibold mb-1 mt-4 text-center text-indigo-600">
+                Ввести потраченную сумму
+              </label>
+              <input
+                name="enterAmount"
+                value={enterAmount}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  // ограничение ввода только цифрами
+                  const pattern = /^[0-9]{1,9}$/;
+                  if (pattern.test(value)) {
+                    setEnterAmount(value);
+                  }
+                }}
+                type="text"
+                className="border pl-2 mt-1 border-solid rounded-md outline-none border-gray-300 mx-auto py-2 focus:border-2 focus:border-sky-500"
+              />
+            </div>
+          </div>
+          <ButtonAdd
+            title="Добавить"
+            type="submit"
+            AddPurchaseFunction={AddPurchaseFunction}
+          />
+          <Error isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default ExpensesForm;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,36 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
-	return <>
-
-		<div className="flex justify-around py-6 bg-indigo-700">
-			<Link to="/" className="text-xl text-indigo-50 font-bold hover:text-amber-500">Главная страница</Link>
-			<Link to="/recentExpenses" className="text-xl text-indigo-50 font-bold hover:text-amber-500">Последние операции/добавить новую покупку</Link>
-			<Link to="/expensesByMonth" className="text-xl text-indigo-50 font-bold hover:text-amber-500">Расходы по месяцам</Link>
-			<Link to="/expensesByCategories" className="text-xl text-indigo-50 font-bold hover:text-amber-500">Расходы по категориям</Link>
-
-		</div>
-
-	</>;
+  return (
+    <>
+      <div className="flex justify-around py-6 bg-indigo-700">
+        <Link
+          to="/"
+          className="text-xl text-indigo-50 font-bold hover:text-amber-500"
+        >
+          Главная страница
+        </Link>
+        <Link
+          to="/recentExpenses"
+          className="text-xl text-indigo-50 font-bold hover:text-amber-500"
+        >
+          Последние операции/добавить новую покупку
+        </Link>
+        <Link
+          to="/expensesByMonth"
+          className="text-xl text-indigo-50 font-bold hover:text-amber-500"
+        >
+          Расходы по месяцам
+        </Link>
+        <Link
+          to="/expensesByCategories"
+          className="text-xl text-indigo-50 font-bold hover:text-amber-500"
+        >
+          Расходы по категориям
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Header;
